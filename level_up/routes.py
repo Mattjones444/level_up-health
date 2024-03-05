9404c97f188a2c5772daa231a5d69ae1cd2202cb
Matt Jones <mattjones444@gmail.com>
2024-03-05 16:44:56 +0000

    Added register profile functionality

--- level_up/routes.py
+++ level_up/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from level_up import app, db
+from flask import request,redirect,url_for
 from level_up.models import Users, Profile, Categories, Hydration_intentions, Exercise_intentions, Sleep_intentions, Mindfulness_intentions
 
 
@@ -7,3 +8,15 @@ from level_up.models import Users, Profile, Categories, Hydration_intentions, Ex
 @app.route("/home_page")
 def home_page():
     return render_template("base.html")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        register = Users(
+            username=request.form.get("username"),
+            password=request.form.get("password")
+        )
+        db.session.add(register)
+        db.session.commit()
+        return redirect(url_for('register'))
+    return render_template("register.html")
